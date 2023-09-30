6f1711ad607577ef8f99432a78689a98a9638813
Pipin Fitriadi <pipinfitriadi@gmail.com>
2023-10-01 05:18:41 +0700

    fix: Remove unused lib

--- asicverifier/restful_api.py
+++ asicverifier/restful_api.py
@@ -3,8 +3,6 @@
 # This module is part of AsicVerifier and is released under
 # the AGPL-3.0-only License: https://opensource.org/license/agpl-v3/
 
-from urllib.parse import urljoin
-
 from fastapi import APIRouter, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
